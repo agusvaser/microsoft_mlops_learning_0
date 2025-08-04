e0fdd0c6b858f066de2788d547d0e35497ad6809
agusvaser <119825300+agusvaser@users.noreply.github.com>
2025-08-04 15:28:09 -0300

    Update train.py
    adding autolog

--- src/model/train.py
+++ src/model/train.py
@@ -7,14 +7,15 @@ import os
 import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
 
+from sklearn.model_selection import train_test_split
+import mlflow
 
 
 # define functions
 def main(args):
     # TO DO: enable autologging
-
+    mlflow.autolog()
 
     # read data
     df = get_csvs_df(args.training_data)
